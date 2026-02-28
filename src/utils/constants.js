b2369ae1e8fc6434da24596cd21af1f58bb240f8
nwtnmusical <nwtnmusical@gmail.com>
2026-02-28 17:16:21 +0530

    Create constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,41 @@
+export const COLORS = {
+  primary: '#d12200',
+  secondary: '#a51502',
+  accent: '#f8c5c0',
+  dark: '#cf2100'
+};
+
+export const SORT_OPTIONS = {
+  NEWEST: 'newest',
+  OLDEST: 'oldest',
+  MOST_PLAYED: 'most_played',
+  MOST_VIEWED: 'most_viewed',
+  MOST_LIKED: 'most_liked'
+};
+
+export const CONTENT_TYPES = {
+  SONG: 'song',
+  VIDEO: 'video'
+};
+
+export const USER_ROLES = {
+  ADMIN: 'admin',
+  USER: 'user'
+};
+
+export const ANALYTICS_ACTIONS = {
+  PLAY: 'play',
+  PAUSE: 'pause',
+  LIKE: 'like',
+  COMMENT: 'comment',
+  SHARE: 'share',
+  VIEW: 'view'
+};
+
+export const API_ENDPOINTS = {
+  SONGS: '/api/songs',
+  VIDEOS: '/api/videos',
+  COMMENTS: '/api/comments',
+  RATINGS: '/api/ratings',
+  ANALYTICS: '/api/analytics'
+};
